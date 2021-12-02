1ba7bbe4dc3f0852045744946f005986f78f1711
mhakbar <habibakbar@live.com>
2021-12-01 18:22:06 -0800

    finally got the snake moving

--- client.js
+++ client.js
@@ -7,6 +7,7 @@ const connect = function () {
     port: 50541 // PORT number here,
   });
 
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
@@ -14,41 +15,14 @@ const connect = function () {
     console.log('disconnected from server', data);
   });
 
-  conn.on('connect', (connect) => {
-    console.log("Successfully connected to game server", connect);
-
-
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
     conn.write('Name: HAM');
 
-
-    // setTimeout(() => {
-    //   conn.write("Move: up");
-    // }, 500);
-
-    // setTimeout(() => {
-    //   conn.write("Move: up");
-    // }, 1000);
-
-    // setTimeout(() => {
-    //   conn.write("Move: up");
-    // }, 1500);
-
-    // setInterval(() => { 
-    //   conn.write("Move: left"); 
-    // }, 3000);
-
-    //use setInterval and setTimeout functions to automatically move your snake. setInterval keeps playing the same command on a loop on a given interval. 
-    
-    //conn.write("Move: down");
-    //conn.write("Move: right");
   });
-
-  // conn.on('move up', (up) => {
-  //   conn.write("Move: up", up);
-  // });
 
 
   return conn;
 };
-
+console.log("Connecting ...");
 module.exports = {connect}
--- play.js
+++ play.js
@@ -20,10 +20,47 @@
 // };
 const {connect} = require("./client");
 
+//connect();
 
-console.log("Connecting ...");
-connect();
+// setup interface to handle user input from stdin
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  
+  return stdin;
+};
+const conn = connect() ;
+const handleUserInput = function (key) {
+
+  if (key === "\u0003") {
+    process.exit();
+  } else if (key === "\u001b[A") {
+    conn.write('Move: up');
+    console.log(`Moved up`);
+  } else if (key === "\u001b[C") {
+    conn.write('Move: right');
+    console.log(`Moved right`);
+  } else if (key === "\u001b[B") {
+    conn.write('Move: down');
+    console.log(`Moved down`);
+
+  } else if (key === "\u001b[D") {
+    conn.write('Move: left');
+    console.log(`Moved left`);
+
+
+  
+  
+
+  // your code here
+};
+};
+
+setupInput();
 
 
 
